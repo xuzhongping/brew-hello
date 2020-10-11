803486553318174d69976c8a94cc4dc6a6805021
徐仲平 <zhongping.xu@ushow.media>
2020-10-11 18:39:38 +0800

    update

---
+++ brew-hello.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  brew-hello
+//
+//  Created by xuzhongping on 2020/10/11.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
