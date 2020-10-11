da3454042a39a566ad663d25d1ed59be173cdcfb
徐仲平 <zhongping.xu@ushow.media>
2020-10-11 18:44:22 +0800

    update

---
+++ brew-hello.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  brew-hello
+//
+//  Created by xuzhongping on 2020/10/11.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
